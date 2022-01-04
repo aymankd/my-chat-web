0e9d4a1515543b2c070f2429c57c936ba65d0175
aymankd <48860272+aymankd@users.noreply.github.com>
2022-01-04 21:32:29 +0100

    first commit

---
+++ src/pages/Auth/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import login from "./login";
+import register from "./register";
+function index() {
+  const [authState, setauthState] = useState(true);
+  return <div>hello</div>;
+}
+export default index;
